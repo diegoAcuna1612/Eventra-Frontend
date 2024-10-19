3fdbc625ab7750ed6698f04d0c8fef352178eefa
JulianaYS <u20201b782@upc.edu.pe>
2024-10-18 20:15:16 -0500

    feat(event): implement choose payment method and card ticket.

---
+++ src/app/home/model/event.ts
@@ -0,0 +1,12 @@
+export interface Event{
+    imageSrc: string;
+    title: string;
+    description: string;
+    startEventDate: string;
+    endEventDate: string;
+    tags: string[];
+    price: number;
+    location:string;
+    rating: number;
+    hour: string;
+}
