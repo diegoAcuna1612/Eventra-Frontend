acc40bc8523d6dad36e1b56e73140f8e33927fef
carlos <U20201C794@upc.edu.pe>
2024-10-24 17:58:57 -0500

    create environment

--- src/app/auth/services/countries.service.ts
+++ src/app/auth/services/countries.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountryService {
   private apiUrl = 'https://api.first.org/data/v1/countries';
-
-  constructor(private http: HttpClient) { }
+  private endpoint='users'
+  private domain: string|undefined;
+  constructor(private http: HttpClient) { 
+      this.domain = environment.domain;
+  }
 
   getCountries(): Observable<any> {
     return this.http.get<any>(this.apiUrl);
