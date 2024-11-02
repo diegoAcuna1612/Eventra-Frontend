5f1d3702b9b6465af5eadb6b87eb95e7a5bae8f1
jackby03 <jackdelaguilavilla03@gmail.com>
2024-11-02 17:02:32 -0500

    deploy url backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment={
-    production:false,
-    domain: 'http://localhost:8080/api/',  
+  production:false,
+  domain: 'http://eventra-backend-982215-9a4f09-35-239-187-59.traefik.me/api/',
 }
