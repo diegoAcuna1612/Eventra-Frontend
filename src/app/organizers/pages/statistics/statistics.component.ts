dd4cf8bd597bc681a53eb7eaadbf18935a6faf3d
JulianaYS <u20201b782@upc.edu.pe>
2024-11-19 16:47:46 -0500

    fix(statistics): fixed view statistics

--- src/app/organizers/pages/statistics/statistics.component.ts
+++ src/app/organizers/pages/statistics/statistics.component.ts
@@ -18,7 +18,7 @@ export class StatisticsComponent implements OnInit{
 
   ngOnInit(): void {
     this.event = {
-      imageUrl: 'https://example.com/event-image.jpg',
+      imageUrl: 'https://static.vecteezy.com/system/resources/previews/026/619/142/non_2x/default-avatar-profile-icon-of-social-media-user-photo-image-vector.jpg',
       name: 'Evento de Ejemplo',
       attendees: 150,
       purchaseRate: '75%',
