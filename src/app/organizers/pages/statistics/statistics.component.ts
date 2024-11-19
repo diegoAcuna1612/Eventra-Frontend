54caf076fd5f59ca7839cbca24be2791b182dd04
JulianaYS <u20201b782@upc.edu.pe>
2024-11-19 11:12:31 -0500

    feat(event): added statistics organizers.

---
+++ src/app/organizers/pages/statistics/statistics.component.ts
@@ -0,0 +1,53 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-statistics',
+  standalone: true,
+  imports: [],
+  templateUrl: './statistics.component.html',
+  styleUrl: './statistics.component.css'
+})
+export class StatisticsComponent implements OnInit{
+  events: any[] = [];
+  //constructor(private eventService: EventService) {}
+
+  event: any;
+
+  ngOnInit(): void {
+    this.event = {
+      imageUrl: 'https://example.com/event-image.jpg',
+      name: 'Evento de Ejemplo',
+      attendees: 150,
+      purchaseRate: '75%',
+      male: 80,
+      female: 70,
+      averageAge: 30,
+      socialLikes: {
+        facebook: 120,
+        twitter: 80,
+        instagram: 200
+      },
+      comparison: [
+        {
+          name: 'Evento Comparado 1',
+          attendees: 100,
+          purchaseRate: '60%',
+          male: 50,
+          female: 50,
+          averageAge: 28,
+          interactions: 300
+        },
+        {
+          name: 'Evento Comparado 2',
+          attendees: 200,
+          purchaseRate: '80%',
+          male: 120,
+          female: 80,
+          averageAge: 32,
+          interactions: 500
+        }
+      ]
+    };
+  }
+}
+
